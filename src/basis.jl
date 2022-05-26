d70c8226c8d1c0556e3e2accbcffb4d0550660e9
Leticia-maria <leticiapequeno30@gmail.com>
2022-05-26 15:04:50 -0300

    DEfine some types

---
+++ src/basis.jl
@@ -0,0 +1,17 @@
+abstract type Basis end
+
+struct GaussianBasis<:Basis
+    atomNo::Int
+    orbital::String
+    coords::Array{Float, 3}
+    l::Int
+    m::Int
+    n::Int
+    a::Array{Float, 3}
+    d::Array{Float, 3}
+end 
+
+function build_sto3Gbasis(molecule::Molecule) -> Basis
+    sto3Gbasis = []
+    K = 0
+end
