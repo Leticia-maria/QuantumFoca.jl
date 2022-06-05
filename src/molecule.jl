22c83a3c41a07266a16c2c25bdbb0b9e4113f588
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-05 20:29:06 -0300

    Simplify the function

--- src/molecule.jl
+++ src/molecule.jl
@@ -1,18 +1,14 @@
 abstract type ChemicalEntity end
-
-abstract type ChemicalFile end
 struct Molecule <: ChemicalEntity
     atoms::Vector{String}
     coords::Matrix{Float64}
 end
 
-struct XYZ{::Molecule} <: ChemicalFile end
-
-function Base.read(xyzfile::IO, ::Type{XYZ{M}}) where {M<:Molecule}
+function molecule(xyzfile::String) :: Molecule
     elements = String[]
-    coordinates = Matrix{Float64}(undef, 3, 0)
+    coordinates = []
 
-    for atom in Iterators.drop(eachline(xyzfile), 2)
+    for line in Iterators.drop(eachline(xyzfile), 2)
         fields = split(line)
 
         element = fields[1]
@@ -20,10 +16,7 @@ function Base.read(xyzfile::IO, ::Type{XYZ{M}}) where {M<:Molecule}
 
         coordinate = parse.(Float64, fields[2:4])
         push!(coordinates, coordinate)
-        coordinates = hcat(coordinates, coordinate)
     end
 
-    return M(elements, coordinates)
+    return Molecule(elements, coordinates)
 end
-
-Base.read(input::IO, ::Type{XYZ{Molecule}}) = read(input, XYZ{Molecule})
