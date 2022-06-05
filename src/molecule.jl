133a76164566bed348224c6d456f50bcec16a606
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-05 18:31:55 -0300

    Modify the function of reading files

--- src/molecule.jl
+++ src/molecule.jl
@@ -1,48 +1,29 @@
 abstract type ChemicalEntity end
+
 abstract type ChemicalFile end
 
-struct XYZfile<:ChemicalFile
-    coordsX::Matrix{Int64}
-    coordsY::Matrix{Int64}
-    coordsZ::Matrix{Int64}
+struct XYZ{M<:Molecule} <: ChemicalFile end
+struct Molecule <: ChemicalEntity
+    atoms::Vector{String}
+    coords::Matrix{Float64}
 end
 
-struct Molecule<:ChemicalEntity
-    numberOfAtoms::Int
-    symbols::Vector{String}
-    coords::Matrix{Int64}
-end
+function Base.read(xyzfile::IO, ::Type{XYZ{M}}) where {M<:Molecule}
+    elements = String[]
+    coordinates = Matrix{Float64}(undef, 3, 0)
 
-"""
-Build 2D nested list containing raw data from .xyz input file.
-"""
-function constructNestedArray(data)
-    n = 0
-    fileNestedlist = Matrix{Int64}
-    open(data) do file
-        for (l, line) in enumerate(eachline(file))
-            if l == 1
-                try
-                    n = parse(Int64, line)
-                catch
-                    print("The xyz file is not in the correct format. Make sure the format follows: https://en.wikipedia.org/wiki/XYZ_file_format")
-                end
-            end
+    for atom in Iterators.drop(eachline(xyzfile), 2)
+        fields = split(line)
 
-            if l  == 2
-                continue
-            end
+        element = fields[1]
+        push!(elements, element)
 
-            if l != 1
-                atom_coordinates = [
-                    split(line)[1],
-                    parse(Float64, split(line)[2]),
-                    parse(Float64, split(line)[3]),
-                    parse(Float64, split(line)[4])
-                ]
-                append!(fileNestedlist, [atom_coordinates])
-            end
-        end
+        coordinate = parse.(Float64, fields[2:4])
+        push!(coordinates, coordinate)
+        coordinates = hcat(coordinates, coordinate)
     end
-    return n, fileNestedlist
+
+    return M(elements, coordinates)
 end
+
+Base.read(input::IO, ::Type{XYZ}) = read(input, XYZ{Molecule})
