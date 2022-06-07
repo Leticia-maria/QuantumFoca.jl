dfedb2949d47cab080e020905aa509b75dd69400
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-07 10:10:01 -0300

    Rename files

--- src/basis.jl
+++ src/basis.jl
@@ -1,18 +1,9 @@
 abstract type Basis end
-struct GaussianBasis<:Basis
-    atomNo::Int
-    orbital::String
-    coords::Array{Float, 3}
-    l::Int
-    m::Int
-    n::Int
-    a::Array{Float, 3}
-    d::Array{Float, 3}
-end 
-
-function build_sto3Gbasis(molecule::Molecule)
-    sto3Gbasis = []
-    K = 0
-
+struct GaussianBasis <: Basis
+    Z::Int64
+    R
+    l
+    m
+    n
+    α
     
-end
