beb672bd5dca9a18c788ff5fced1c1ecfd6e01d6
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-07 11:50:00 -0300

    Format basis set

--- src/basis.jl
+++ src/basis.jl
@@ -1,13 +1,67 @@
 abstract type Basis end
 struct GaussianBasis <: Basis
-    Z::Int64
-    R::Int64
+    R::Vector{Float64}
+    α::Vector{Float64}
+    dₐ::Vector{Float64}
     l::Int64
     m::Int64
     n::Int64
-    α::Vector{Float64}
 end
 
-function basis(molecule::Molecule)
-    
+function buildbasis(molecule::Molecule)
+    sto3g = GaussianBasis[]
+
+    for i in eachindex(molecule.atoms)
+        number = molecule.numbers[i]
+        coord = molecule.coords[i, :]
+
+        for orbital in orbitalconfig(number)
+            if orbital == "1s"
+                push!(sto3g, GaussianBasis(
+                    coord,
+                    sto3g_α[number][1, :],
+                    sto3g_dₐ[1, :],
+                    0,
+                    0,
+                    0
+                ))
+            elseif orbital == "2s"
+                push!(sto3g, GaussianBasis(
+                    coord,
+                    sto3g_α[number][2, :],
+                    sto3g_dₐ[2, :],
+                    0,
+                    0,
+                    0
+                ))
+            elseif orbital == "2p"
+                push!(sto3g, GaussianBasis(
+                    coord,
+                    sto3g_α[number][2, :],
+                    sto3g_dₐ[3, :],
+                    1,
+                    0,
+                    0
+                ))
+                push!(sto3g, GaussianBasis(
+                    coord,
+                    sto3g_α[number][2, :],
+                    sto3g_dₐ[3, :],
+                    0,
+                    1,
+                    0
+                ))
+                push!(sto3g, GaussianBasis(
+                    coord,
+                    sto3g_α[number][2, :],
+                    sto3g_dₐ[3, :],
+                    0, # pₓ
+                    0,
+                    1
+                ))
+            end
+        end
+    end
+
+    return sto3g
 end
