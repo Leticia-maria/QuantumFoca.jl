14b51eb599a071c2ea9bd9056c83cce6f419f1a9
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-07 10:20:26 -0300

    Correct the syntax error

--- src/basis.jl
+++ src/basis.jl
@@ -1,9 +1,10 @@
 abstract type Basis end
 struct GaussianBasis <: Basis
     Z::Int64
-    R
-    l
-    m
-    n
-    α
+    R::Int64
+    l::Int64
+    m::Int64
+    n::Int64
+    α::Vector{Float64}
+end
     
