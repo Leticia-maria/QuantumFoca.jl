a7a5a059cdd8747ac3b9d102831b3f8ebbf91ad1
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-07 11:50:17 -0300

    Include files and export types and functions

--- src/Foca.jl
+++ src/Foca.jl
@@ -1,6 +1,12 @@
 module Foca
     include("atoms.jl")
     include("molecule.jl")
+    include("sto3g.jl")
+    include("basis.jl")
+    
     export Molecule
     export molecule
+
+    export GaussianBasis
+    export buildbasis
 end
