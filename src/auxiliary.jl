1d73101c19969e0f2abb3800364a7c22d8da2bc0
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-08 20:00:50 -0300

    Add gaussian norm

--- src/auxiliary.jl
+++ src/auxiliary.jl
@@ -19,3 +19,14 @@ function doublefactorial(number)
     end
     return fact
 end
+
+"""
+gaussian_norm(α, l, m, n)
+Compute the normalization constant for a Gaussian primitive basis function.
+"""
+@inline function gaussian_norm(α, l::Int, m::Int, n::Int)
+    N = sqrt((2α / π)^3) * (4α)^(l + m + n)
+    κ = doublefactorial(2l - 1) * doublefactorial(2m - 1) * doublefactorial(2n - 1)
+
+    return sqrt(N / κ)
+end
