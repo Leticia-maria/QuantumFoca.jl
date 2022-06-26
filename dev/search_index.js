cfa8398deaeff4fbd26fd9b2811f7004f0a62d6f
Documenter.jl <documenter@juliadocs.github.io>
2022-06-26 13:38:27 +0000

    build based on 7ce6f22

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VegaGraphs.jl","page":"Home","title":"VegaGraphs.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The best summation package.","category":"page"},{"location":"#Package-Features","page":"Home","title":"Package Features","text":"","category":"section"}]
+}
