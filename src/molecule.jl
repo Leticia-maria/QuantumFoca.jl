7145a22a036b9b356a6f288f6212ddb6d7441d00
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-26 16:20:37 -0300

    Add docstrings for molecules

--- src/molecule.jl
+++ src/molecule.jl
@@ -1,9 +1,31 @@
+""" 
+```ChemicalEntity``` is an *abstract type* that englobes ```Molecule```, ```Atom```, ```AtomicOrbital``` and ```MolecularOrbital``` structures.
+"""
 abstract type ChemicalEntity end
+
+"""
+```Molecule``` is a *subtype* of ```ChemicalEntity``` that stores coordinates, atomic symbols and atomic numbers as objects.
+"""
 struct Molecule <: ChemicalEntity
     atoms::Vector{String}
     coords::Matrix{Float64}
     numbers::Vector{Int64}
 end
+
+
+"""
+This method takes an ```.xyz``` file (with cartesian coordinates of atoms in a molecule) and returns a ```Molecule```. The ```.xyz``` file should be formatted as follows
+
+```julia
+2 
+
+H      -1.788131055      0.000000000     -4.028513155
+H      -1.331928651      0.434077746     -3.639854078
+```
+
+In the first line, the file should contain the numer of atoms that are in the molecule. In the second line, there is a comment, which can be the *name of the compound*, 
+*molecular formula*, etc. To further information about ```.xyz``` files, [*click here*](https://www.reviversoft.com/file-extensions/xyz).
+"""
 
 function molecule(xyzfile::String) :: Molecule
     elements = String[]
