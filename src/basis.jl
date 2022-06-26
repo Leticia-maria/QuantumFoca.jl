9e99f54ef7c7a71df72a5fc37b7609464db3d658
Leticia-maria <leticiapequeno30@gmail.com>
2022-06-26 17:27:56 -0300

    Add a docstring for buildbasis method

--- src/basis.jl
+++ src/basis.jl
@@ -23,6 +23,23 @@ struct GaussianBasis <: Basis
     n::Int64
 end
 
+"""
+The ```buildbasis``` method takes a ```Molecule``` as input and return for us a ```GaussianBasis```. For example, let's open the same 
+```h2.xyz``` example. As a standard basis set, we use STO-3G data.
+
+```julia
+2 
+
+H      -1.788131055      0.000000000     -4.028513155
+H      -1.331928651      0.434077746     -3.639854078
+```
+We give the file as an input:
+
+```julia
+hydrogen = molecule("h2.xyz")
+sto3g = buildbasis(hydrogen)
+```
+"""
 function buildbasis(molecule::Molecule)
     sto3g = GaussianBasis[]
 
